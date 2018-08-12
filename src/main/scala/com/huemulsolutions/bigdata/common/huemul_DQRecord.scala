6822d86185fa0cb87f8655e424ac1d581ead7694
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2018-08-12 20:49:23 -0300

    Lista de DQ aplicada

---
+++ src/main/scala/com/huemulsolutions/bigdata/common/huemul_DQRecord.scala
@@ -0,0 +1,22 @@
+package com.huemulsolutions.bigdata.common
+
+import org.apache.spark.sql.types._
+
+
+class huemul_DQRecord {
+  var Table_Name: String = null
+  var BBDD_Name: String= null
+  var DF_Alias: String= null
+  var ColumnName: String= null
+  var DQ_Name: String= null
+  var DQ_Description: String= null
+  var DQ_IsAggregate: Boolean= false
+  var DQ_RaiseError: Boolean= true
+  var DQ_SQLFormula: String= null
+  var DQ_Error_MaxNumRows: Long= 0
+  var DQ_Error_MaxPercent: Decimal= null
+  var DQ_ResultDQ: String= null
+  var DQ_NumRowsOK: Long = 0
+  var DQ_NumRowsError: Long= 0
+  var DQ_NumRowsTotal: Long= 0
+}
