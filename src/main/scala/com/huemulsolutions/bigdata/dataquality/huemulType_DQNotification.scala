734ee1ad3161b98b4109835e623ed0bde5da1d26
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2018-08-27 23:33:19 -0300

    Agrega tipos de datos para identificar nivel de query y notificación DQ

---
+++ src/main/scala/com/huemulsolutions/bigdata/dataquality/huemulType_DQNotification.scala
@@ -0,0 +1,7 @@
+package com.huemulsolutions.bigdata.dataquality
+
+object huemulType_DQNotification extends Enumeration {
+  type huemulType_DQNotification = Value
+  val ERROR, WARNING = Value
+}
+
