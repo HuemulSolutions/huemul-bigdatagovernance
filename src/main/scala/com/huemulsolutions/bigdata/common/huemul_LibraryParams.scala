5a46fa2e8b5a4368332071102f43bbcb6a9b2e49
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2018-10-01 18:19:19 -0300

    Agrega cálculo de diferencia de fechas como una nueva clase

--- src/main/scala/com/huemulsolutions/bigdata/common/huemul_LibraryParams.scala
+++ src/main/scala/com/huemulsolutions/bigdata/common/huemul_LibraryParams.scala
@@ -5,3 +5,15 @@ class huemul_LibraryParams() extends Serializable {
   var param_value: String = null
   var param_type: String = null //function, program
 }
+
+class huemul_DateTimePart(MiliSeconds: Long) extends Serializable {
+  private var calc = MiliSeconds / 1000
+  val second = calc % 60
+  calc /= 60
+  val minute = calc % 60
+  calc /= 60
+  val hour = calc % 24
+  calc /= 24
+  val days = calc
+    
+}
