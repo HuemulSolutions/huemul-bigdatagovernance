5cf484f7547078591ecc55aea3fead575b1294f2
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2019-03-03 20:07:59 -0300

    Agrega estructura para almacenar detalle de errores en DQ

--- src/main/scala/com/huemulsolutions/bigdata/dataquality/huemul_DataQualityResult.scala
+++ src/main/scala/com/huemulsolutions/bigdata/dataquality/huemul_DataQualityResult.scala
@@ -2,12 +2,25 @@ package com.huemulsolutions.bigdata.dataquality
 
 import org.apache.spark.sql._
 import scala.collection.mutable._
+import org.apache.spark.sql.types._
+import com.huemulsolutions.bigdata.tables._
 
 class huemul_DataQualityResult extends Serializable {
   var isError: Boolean = false
   var Error_Code: Integer = null
   var Description: String = ""
   var dqDF : DataFrame = null
+  var DetailErrorsDF: DataFrame = null
+  
+  
+  val _control_id = new huemul_Columns (StringType, false, "DQ Id partition (control_id)", false)
+  val _error_columnname = new huemul_Columns (StringType, false, "DQ Column Name ", false)
+  val _error_notification = new huemul_Columns (StringType, false, "DQ Notification", false)
+  val _error_code = new huemul_Columns (StringType, false, "DQ Error Code", false)
+  val _error_descripcion = new huemul_Columns (StringType, false, "DQ User error descripcion", false)
+  
+  
+  
     
   var profilingResult: huemul_Profiling = new huemul_Profiling()
   private var DQ_Result: ArrayBuffer[huemul_DQRecord] = new ArrayBuffer[huemul_DQRecord]()
