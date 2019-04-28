f6d5d8d0df8ceb19f1450a12cc490219c44db7c3
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2019-04-27 22:14:07 -0300

    Agrega N° particiones a DF_FROM_SQL

--- src/main/scala/com/huemulsolutions/bigdata/common/huemul_DataFrame.scala
+++ src/main/scala/com/huemulsolutions/bigdata/common/huemul_DataFrame.scala
@@ -90,9 +90,16 @@ class huemul_DataFrame(huemulBigDataGov: huemul_BigDataGovernance, Control: huem
   /**   
    Create DF from SQL (equivalent to spark.sql method)
    */
-  def DF_from_SQL(Alias: String, sql: String, SaveInTemp: Boolean = true) {
+  def DF_from_SQL(Alias: String, sql: String, SaveInTemp: Boolean = true, NumPartitions: Integer = null) {
     if (huemulBigDataGov.DebugMode && !huemulBigDataGov.HideLibQuery) println(sql)
-    val DFTemp = huemulBigDataGov.spark.sql(sql)
+    
+    
+    //val DFTemp = huemulBigDataGov.spark.sql(sql)
+                 
+                 
+    val DFTemp = if (NumPartitions == null || NumPartitions <= 0) huemulBigDataGov.spark.sql(sql)
+                 else huemulBigDataGov.spark.sql(sql).repartition(NumPartitions)
+                 
     
     setDataFrame(DFTemp, Alias, SaveInTemp)
         
