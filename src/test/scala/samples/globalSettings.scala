ffc112e5d4782866a3767c0b23f67466f9bce38f
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2019-06-10 22:55:04 -0400

    #38 agrega nuevos parámetros requeridos

--- src/test/scala/samples/globalSettings.scala
+++ src/test/scala/samples/globalSettings.scala
@@ -84,5 +84,12 @@ object globalSettings {
    Global.DQError_Path.append(new huemul_KeyValuePath("production","hdfs:///user/data/production/dqerror/"))
    Global.DQError_Path.append(new huemul_KeyValuePath("experimental","hdfs:///user/data/experimental/dqerror/"))
 
+   Global.MDM_SaveOldValueTrace = true
+   Global.MDM_OldValueTrace_DataBase.append(new huemul_KeyValuePath("production","production_mdm_oldvalue"))
+   Global.MDM_OldValueTrace_DataBase.append(new huemul_KeyValuePath("experimental","experimental_mdm_oldvalue"))
+   
+   Global.MDM_OldValueTrace_Path.append(new huemul_KeyValuePath("production","hdfs:///user/data/production/mdm_oldvalue/"))
+   Global.MDM_OldValueTrace_Path.append(new huemul_KeyValuePath("experimental","hdfs:///user/data/experimental/mdm_oldvalue/"))
+
 }
 
