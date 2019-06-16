9c5562bbf1ac0fa1062d816aea00288bee48bffe
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2019-06-16 17:59:18 -0400

    #38 agrega tipo huemulType_InternalTableType

--- src/main/scala/com/huemulsolutions/bigdata/tables/huemulType_Tables.scala
+++ src/main/scala/com/huemulsolutions/bigdata/tables/huemulType_Tables.scala
@@ -4,3 +4,9 @@ object huemulType_Tables extends Enumeration {
   type huemulType_Tables = Value
   val Reference, Master, Transaction = Value
 }
+
+
+object huemulType_InternalTableType extends Enumeration {
+  type huemulType_InternalTableType = Value
+  val Normal, DQ, OldValueTrace = Value
+}
