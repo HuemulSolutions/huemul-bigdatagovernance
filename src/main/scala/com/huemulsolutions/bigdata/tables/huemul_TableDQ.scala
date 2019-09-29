9a705bddda331a6ce591e3f84339cb4f900ae5d9
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2019-09-29 12:30:21 -0300

    #60 modificación de estructura de tabla xx_dq, incluye dq_error_id

--- src/main/scala/com/huemulsolutions/bigdata/tables/huemul_TableDQ.scala
+++ src/main/scala/com/huemulsolutions/bigdata/tables/huemul_TableDQ.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.types._
 
 class huemul_TableDQ extends Serializable  {
   val dq_control_id = new huemul_Columns (StringType, false, "DQ Id partition (control_id)", false)
+  val dq_error_id = new huemul_Columns (StringType, false, "Id DQ (dq_id in control_dq table)", false)
   val dq_error_columnname = new huemul_Columns (StringType, false, "DQ Column Name ", false)
   val dq_error_notification = new huemul_Columns (StringType, false, "DQ Notification", false)
   val dq_error_code = new huemul_Columns (StringType, false, "DQ Error Code", false)
