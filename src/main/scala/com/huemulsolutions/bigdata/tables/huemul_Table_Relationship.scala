2de52de4f7bc6c6910f150650bc3a3dd02a52f6b
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2019-10-22 17:21:58 -0300

    #58 Agrega notificacion y #63 agrega Broadcast en FK

--- src/main/scala/com/huemulsolutions/bigdata/tables/huemul_Table_Relationship.scala
+++ src/main/scala/com/huemulsolutions/bigdata/tables/huemul_Table_Relationship.scala
@@ -2,6 +2,8 @@ package com.huemulsolutions.bigdata.tables
 
 import scala.collection.mutable.ArrayBuffer
 import com.huemulsolutions.bigdata.common.huemul_BigDataGovernance
+import com.huemulsolutions.bigdata.dataquality._
+import com.huemulsolutions.bigdata.dataquality.huemulType_DQNotification.huemulType_DQNotification
 
 class huemul_Table_RelationshipColumns (pk: huemul_Columns, fk: huemul_Columns) {
   var PK: huemul_Columns = pk
@@ -16,6 +18,20 @@ class huemul_Table_Relationship(Class_TableName: Object, allowNull: Boolean) {
       Relationship.append(new huemul_Table_RelationshipColumns(PK, FK) )
   }
   val _Class_TableName: Object = Class_TableName
+ 
+  private var _Notification: com.huemulsolutions.bigdata.dataquality.huemulType_DQNotification.huemulType_DQNotification = huemulType_DQNotification.ERROR
+  def setNotification(value: huemulType_DQNotification ): huemul_Table_Relationship = {
+    _Notification = value
+    this
+  } 
+  def getNotification(): huemulType_DQNotification = {return _Notification}
   
+  //From 2.1 --> apply broadcast
+  private var _BroadcastJoin: Boolean = false
+  def broadcastJoin(value: Boolean ): huemul_Table_Relationship = {
+    _BroadcastJoin = value
+    this
+  } 
+  def getBroadcastJoin(): Boolean = {return _BroadcastJoin}
   
 }
