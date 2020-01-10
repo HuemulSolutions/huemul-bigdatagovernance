e1f587515d0e68a2f88be92bbdc604fcf7632b36
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-01-10 00:25:53 -0300

    #87 agrega nuevas clases para configuracion de createExternalTable

--- src/main/scala/com/huemulsolutions/bigdata/common/huemul_ExternalDB.scala
+++ src/main/scala/com/huemulsolutions/bigdata/common/huemul_ExternalDB.scala
@@ -1,6 +1,6 @@
 package com.huemulsolutions.bigdata.common
 
-object huemulType_ExternalDB extends Enumeration {
-  type huemulType_ExternalDB = Value
-  val SPARK,HIVE = Value
+class huemul_ExternalDB() extends Serializable {
+    var externalTableUsing_SPARK: huemul_ExternalDBType = new huemul_ExternalDBType().setActive(true).setActiveForHBASE(true)
+    var externalTableUsing_HIVE: huemul_ExternalDBType = new huemul_ExternalDBType()
 }
---
+++ src/main/scala/com/huemulsolutions/bigdata/common/huemul_ExternalDBType.scala
@@ -0,0 +1,58 @@
+package com.huemulsolutions.bigdata.common
+
+import scala.collection.mutable.ArrayBuffer
+import com.huemulsolutions.bigdata.control.huemul_JDBCProperties
+
+class huemul_ExternalDBType() extends Serializable {
+  private var _active: Boolean = false
+  def getActive(): Boolean = {return _active}
+  def setActive(value: Boolean): huemul_ExternalDBType = {
+    _active = value
+    this
+  }
+  
+  private var _activeForHBASE: Boolean = false
+  def getActiveForHBASE(): Boolean = {return _activeForHBASE}
+  def setActiveForHBASE(value: Boolean): huemul_ExternalDBType = {
+    _activeForHBASE = value
+    this
+  }
+  
+  private var _JDBCdriver: String = null
+  def getJDBCdriver(): String = {return _JDBCdriver}
+  def setJDBCdriver(value: String): huemul_ExternalDBType = {
+    _JDBCdriver = value
+    this
+  }
+  
+  private var _connectionStrings: ArrayBuffer[huemul_KeyValuePath] = null
+  def getConnectionStrings(): ArrayBuffer[huemul_KeyValuePath] = {return _connectionStrings}
+  def setConnectionStrings(value: ArrayBuffer[huemul_KeyValuePath]): huemul_ExternalDBType = {
+    _connectionStrings = value
+    this
+  }
+  
+  @transient private var JDBC_connection: huemul_JDBCProperties = null
+  def getJDBC_connection(huemulBigDataGov: huemul_BigDataGovernance): huemul_JDBCProperties = {
+    if (JDBC_connection != null)
+      return JDBC_connection
+      
+    if (getActive() == false && getActiveForHBASE() == false)
+      return null
+      
+    if (getConnectionStrings == null)
+      return null
+
+    if (!huemulBigDataGov.GlobalSettings.ValidPath(getConnectionStrings, huemulBigDataGov.Environment)) {
+      huemulBigDataGov.logMessageWarn("JDBC Connection not defined")
+      return null
+    }
+    
+    val _connString: String = huemulBigDataGov.GlobalSettings.GetPath(huemulBigDataGov, getConnectionStrings)
+    JDBC_connection = new huemul_JDBCProperties(huemulBigDataGov, _connString,getJDBCdriver, huemulBigDataGov.DebugMode)
+    
+    return JDBC_connection
+  }
+  //(this, _HIVE_connString,null, DebugMode) //Connection = null
+ 
+}
