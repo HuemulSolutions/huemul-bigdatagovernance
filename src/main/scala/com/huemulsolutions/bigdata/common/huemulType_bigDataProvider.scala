d44549e8cc8d5520b922c6188a86b299fa15144b
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-03-09 01:56:56 -0300

    Crea enumeración para identificar el proveedor de ecosistema bigData

---
+++ src/main/scala/com/huemulsolutions/bigdata/common/huemulType_bigDataProvider.scala
@@ -0,0 +1,7 @@
+package com.huemulsolutions.bigdata.common
+
+object huemulType_bigDataProvider extends Enumeration {
+  type huemulType_bigDataProvider = Value
+  val None, databricks = Value
+}
+
--- src/main/scala/com/huemulsolutions/bigdata/common/huemulType_cloudProvider.scala
+++
@@ -1,7 +0,0 @@
-package com.huemulsolutions.bigdata.common
-
-object huemulType_cloudProvider extends Enumeration {
-  type huemulType_cloudProvider = Value
-  val None, azure_databricks = Value
-}
-
