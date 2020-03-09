8522fd1633b6975a75bbce0030efa871de4e34ac
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-03-08 22:46:19 -0300

    Agrega clase huemulType_cloudProvider con opciones de nube

---
+++ src/main/scala/com/huemulsolutions/bigdata/common/huemulType_cloudProvider.scala
@@ -0,0 +1,7 @@
+package com.huemulsolutions.bigdata.common
+
+object huemulType_cloudProvider extends Enumeration {
+  type huemulType_cloudProvider = Value
+  val None, azure_databricks = Value
+}
+
