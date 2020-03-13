a2c4eece2b97c2d6c5f81724d566d342b780ac57
Christian Sattler <christian-sattler@users.noreply.github.com>
2020-03-13 13:42:00 -0500

    Update huemulType_FileType.scala
    Agregargo Soporte a Avro para archivos Raw

--- src/main/scala/com/huemulsolutions/bigdata/datalake/huemulType_FileType.scala
+++ src/main/scala/com/huemulsolutions/bigdata/datalake/huemulType_FileType.scala
@@ -2,6 +2,6 @@ package com.huemulsolutions.bigdata.datalake
 
 object huemulType_FileType extends Enumeration {
   type huemulType_FileType = Value
-  val TEXT_FILE, PDF_FILE = Value
+  val TEXT_FILE, PDF_FILE, AVRO_FILE = Value
   
 }
