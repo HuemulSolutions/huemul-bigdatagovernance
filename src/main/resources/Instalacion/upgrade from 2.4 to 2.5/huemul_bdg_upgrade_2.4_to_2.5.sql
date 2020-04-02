d396aac0a644aeb0bfdd8e63bd02554b37e70682
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-04-02 01:25:14 -0300

    #107 agrega columna column_datatypedeploy en control_columns

---
+++ src/main/resources/Instalacion/upgrade from 2.4 to 2.5/huemul_bdg_upgrade_2.4_to_2.5.sql
@@ -0,0 +1,14 @@
+
+ ALTER TABLE control_columns add column_datatypedeploy			  varchar(50);
+ 
+ UPDATE control_columns SET column_datatypedeploy = column_datatype WHERE column_datatypedeploy IS NULL
+ 
+ 
+ UPDATE control_config
+ SET version_mayor	= 2
+    ,version_minor	= 5
+    ,version_patch  = 0
+where config_id = 1;
+
+
+ 
