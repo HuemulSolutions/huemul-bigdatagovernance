b42e1401fa3683f79f7cc69a52a1710ec99f4254
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-04-06 02:34:39 -0400

    #109 Se agrega nueva opción para conexión con HWC

--- src/main/scala/com/huemulsolutions/bigdata/common/huemul_ExternalDB.scala
+++ src/main/scala/com/huemulsolutions/bigdata/common/huemul_ExternalDB.scala
@@ -3,4 +3,7 @@ package com.huemulsolutions.bigdata.common
 class huemul_ExternalDB() extends Serializable {
     var Using_SPARK: huemul_ExternalDBType = new huemul_ExternalDBType().setActive(true).setActiveForHBASE(false)
     var Using_HIVE: huemul_ExternalDBType = new huemul_ExternalDBType()
+    var Using_HWC: huemul_ExternalDBType = new huemul_ExternalDBType()
+    
+    
 }
