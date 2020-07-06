6345e00222158f3c6ec62faffd3847e51c79b724
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-07-06 19:28:41 -0400

    #119 separa clases

---
+++ src/main/scala/com/huemulsolutions/bigdata/datalake/huemul_DataLakeColumns.scala
@@ -0,0 +1,22 @@
+package com.huemulsolutions.bigdata.datalake
+
+import org.apache.spark.sql.types.{DataType, StringType}
+
+class huemul_DataLakeColumns(columnName_Business: String
+                             , columnName_TI: String
+                             , DataType: DataType = StringType
+                             , Description: String = null
+                             , PosIni: Integer = null
+                             , PosFin: Integer = null
+                             , ApplyTrim: Boolean = false
+                             , ConvertToNull: Boolean = false) extends Serializable {
+  def getcolumnName_Business: String =  columnName_Business
+  def getcolumnName_TI: String  =  columnName_TI
+  def getDataType: DataType =  DataType
+  def getDescription: String =  Description
+  def getPosIni: Integer  =  PosIni
+  def getPosFin: Integer  =  PosFin
+  def getApplyTrim: Boolean =  ApplyTrim
+  def getConvertToNull: Boolean  =  ConvertToNull
+
+}
