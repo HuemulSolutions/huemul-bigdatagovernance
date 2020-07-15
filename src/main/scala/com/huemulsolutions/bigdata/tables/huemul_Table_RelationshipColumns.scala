6fe18776bd8d56926664447aff07b84f8f072ddf
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2020-07-15 16:10:29 -0400

    separación de clases

---
+++ src/main/scala/com/huemulsolutions/bigdata/tables/huemul_Table_RelationshipColumns.scala
@@ -0,0 +1,6 @@
+package com.huemulsolutions.bigdata.tables
+
+class huemul_Table_RelationshipColumns (pk: huemul_Columns, fk: huemul_Columns) {
+  var PK: huemul_Columns = pk
+  var FK: huemul_Columns = fk
+}
